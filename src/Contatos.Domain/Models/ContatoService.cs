d09c5e3f1314a6b07ca6716982c1369506cf77c5
Wagner <wagner_silvajr@hotmail.com>
2023-05-18 13:53:57 -0300

    feat: saved ContatoService

--- src/Contatos.Domain/Models/ContatoService.cs
+++ src/Contatos.Domain/Models/ContatoService.cs
@@ -7,6 +7,26 @@ namespace Contatos.Domain.Models
 {
     public class ContatoService
     {
-        
+        private readonly IRepository<Contato> _contatoRepository;
+
+        public ContatoService(IRepository<Contato> _contatoRepository)
+        {
+            _contatoRepository = contatoRepository;
+        }
+
+        public void Save(int id, string nome, string email)
+        {
+            var contato = _contatoRepository.GetById(id);
+
+            if(contato == null)
+            {
+                contato = new Contato(nome, email);
+                _contatoRepository.Save(contato);
+            }
+            else
+                contato.Update(nome, email);
+        }
+
+
     }
 }
