0714294d41675fb8facb6ec31d11e00539320a9c
Wagner <wagner_silvajr@hotmail.com>
2023-05-22 20:15:19 -0300

    feat: created Repository and ContatoRepository in Repositories folder

--- src/Contatos.Infra/Context/AppDbContext.cs
+++ src/Contatos.Infra/Context/AppDbContext.cs
@@ -2,11 +2,16 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Contatos.Domain.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Contatos.Infra.Context
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext
     {
-        
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {            
+        }
+        public DbSet<Contato> Contatos { get; set; }
     }
 }
