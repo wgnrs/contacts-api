6d77c4f5ab0bbb408dedf768f57b5f949f70ad8e
Wagner <wagner_silvajr@hotmail.com>
2023-05-23 17:44:14 -0300

    feat: created repository of UnitOfWork

---
+++ src/Contatos.Infra/Repositories/UnitOfWork.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Contatos.Infra.Repositories
+{
+    public class UnitOfWork
+    {
+        
+    }
+}
