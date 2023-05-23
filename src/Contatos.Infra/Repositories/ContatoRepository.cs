8654c748e167339b6879fe1cae9e0a9b9ecd5b9e
Wagner <wagner_silvajr@hotmail.com>
2023-05-23 17:45:02 -0300

    fix: code was saved

--- src/Contatos.Infra/Repositories/ContatoRepository.cs
+++ src/Contatos.Infra/Repositories/ContatoRepository.cs
@@ -1,12 +1,27 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
-
+using Contatos.Domain.Models;
+using Contatos.Infra.Context;
 namespace Contatos.Infra.Repositories
 {
-    public class ContatoRepository
+    public class ContatoRepository : Repository<Contato>
     {
-        
+        public ContatoRepository(AppDbContext context) : base(context)
+        {            
+        }
+
+        public override Contato GetById(int id)
+        {
+            var query = _context.Set<Contato>().Where(e => e.Id == id);
+            if(query.Any())
+                return query.First();
+            return null;
+        }
+
+        public override IEnumerable<Contato> GetAll()
+        {
+            var query = _context.Set<Contato>();
+            return query.Any() ? query.ToList() : new List<Contato>();
+        }
     }
 }
--- src/Contatos.Infra/Repositories/UnitOfWork.cs
+++ src/Contatos.Infra/Repositories/UnitOfWork.cs
@@ -1,12 +1,20 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
-
+using Contatos.Domain.Interfaces;
+using Contatos.Infra.Context;
 namespace Contatos.Infra.Repositories
 {
-    public class UnitOfWork
+    public class UnitOfWork : IUnitOfWork
     {
-        
+        private readonly AppDbContext _context;
+
+        public UnitOfWork(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task Commit()
+        {
+            await _context.SaveChangesAsync();
+        }
     }
 }
